d0039679faaf2b3a67ea255bfd3a80bb78e5c09e
Cave-Johnson46 <rjcox46@yahoo.com>
2026-04-04 19:04:02 -0500

    update

--- nvim/.config/nvim/lua/config/keybinds.lua
+++ nvim/.config/nvim/lua/config/keybinds.lua
@@ -1,6 +1,10 @@
--- vim.keymap.set("n", "<C-h>", "<C-w>H", { desc = "Move window to the left" })
--- vim.keymap.set("n", "<C-l>", "<C-w>L", { desc = "Move window to the right" })
--- vim.keymap.set("n", "<C-j>", "<C-w>J", { desc = "Move window to the lower" })
--- vim.keymap.set("n", "<C-k>", "<C-w>K", { desc = "Move window to the upper" })
-
 vim.keymap.set("n", "<leader>ft", ":Neotree filesystem reveal left<CR>")
+
+vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = false })
+vim.api.nvim_set_keymap("i", "kk", "<Esc>", { noremap = false })
+
+vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
