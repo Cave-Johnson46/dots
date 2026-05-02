5d856f787d9e426872ccb16fc3bcb534e7c2ada4
Randal Cox <randalcox@Randals-Laptop.local>
2026-05-01 22:47:23 -0500

    Update

--- nvim/.config/nvim/lua/config/keybinds.lua
+++ nvim/.config/nvim/lua/config/keybinds.lua
@@ -4,6 +4,8 @@ vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
 vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = false })
 vim.api.nvim_set_keymap("i", "kk", "<Esc>", { noremap = false })
 
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
 vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
@@ -16,5 +18,5 @@ vim.keymap.set("i", "{<cr>", "{<cr>}<ESC>kA<CR>", {})
 closing_pairs = { "}", ")", "]" }
 opening_pairs = { "{", "(", "[" }
 for key, chr in pairs(opening_pairs) do
-  vim.keymap.set("i", chr, chr .. closing_pairs[key] .. "<esc>i", {})
+	vim.keymap.set("i", chr, chr .. closing_pairs[key] .. "<esc>i", {})
 end
--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -6,7 +6,7 @@ return {
 			ensure_installed = {
 				"lua_ls",
 				"rust_analyzer",
-				"markdown_oxide",
+				"marksman",
 			},
 		},
 		dependencies = {
