4d1f61a61e0ef33c9bc79d2bc57c6fc7d41adcd6
Cave-Johnson46 <rjcox46@yahoo.com>
2026-05-14 20:44:59 -0500

    update

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,66 +1,66 @@
 return {
-	--	"nvim-neo-tree/neo-tree.nvim",
-	--	branch = "v3.x",
-	--	dependencies = {
-	--		"nvim-lua/plenary.nvim",
-	--		"MunifTanjim/nui.nvim",
-	--		"nvim-tree/nvim-web-devicons", -- optional, but recommended
-	--	},
-	--	lazy = false, -- neo-tree will lazily load itself
-	--	opts = {
-	--		event_handlers = {
-	--			{
-	--				event = "file_open_requested",
-	--				handler = function()
-	--					-- auto close
-	--					-- vim.cmd("Neotree close")
-	--					-- OR
-	--					require("neo-tree.command").execute({ action = "close" })
-	--				end,
-	--			},
-	--			{
-	--				event = "after_render",
-	--				handler = function(state)
-	--					if not require("neo-tree.sources.common.preview").is_active() then
-	--						state.config = { use_float = true }
-	--						state.commands.toggle_preview(state)
-	--					end
-	--				end,
-	--			},
-	--		},
-	--		filesystem = {
-	--			components = {
-	--				harpoon_index = function(config, node, _)
-	--					local harpoon_list = require("harpoon"):list()
-	--					local path = node:get_id()
-	--					local harpoon_key = vim.uv.cwd()
-	--
-	--					for i, item in ipairs(harpoon_list.items) do
-	--						local value = item.value
-	--						if string.sub(item.value, 1, 1) ~= "/" then
-	--							value = harpoon_key .. "/" .. item.value
-	--						end
-	--
-	--						if value == path then
-	--							vim.print(path)
-	--							return {
-	--								text = string.format(" ⥤ %d", i), -- <-- Add your favorite harpoon like arrow here
-	--								highlight = config.highlight or "NeoTreeDirectoryIcon",
-	--							}
-	--						end
-	--					end
-	--					return {}
-	--				end,
-	--			},
-	--			renderers = {
-	--				file = {
-	--					{ "icon" },
-	--					{ "name", use_git_status_colors = true },
-	--					{ "harpoon_index" }, --> This is what actually adds the component in where you want it
-	--					{ "diagnostics" },
-	--					{ "git_status", highlight = "NeoTreeDimText" },
-	--				},
-	--			},
-	--		},
-	--	},
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		"nvim-tree/nvim-web-devicons", -- optional, but recommended
+	},
+	lazy = false, -- neo-tree will lazily load itself
+	opts = {
+		event_handlers = {
+			{
+				event = "file_open_requested",
+				handler = function()
+					-- auto close
+					-- vim.cmd("Neotree close")
+					-- OR
+					require("neo-tree.command").execute({ action = "close" })
+				end,
+			},
+			{
+				event = "after_render",
+				handler = function(state)
+					if not require("neo-tree.sources.common.preview").is_active() then
+						state.config = { use_float = true }
+						state.commands.toggle_preview(state)
+					end
+				end,
+			},
+		},
+		filesystem = {
+			components = {
+				harpoon_index = function(config, node, _)
+					local harpoon_list = require("harpoon"):list()
+					local path = node:get_id()
+					local harpoon_key = vim.uv.cwd()
+
+					for i, item in ipairs(harpoon_list.items) do
+						local value = item.value
+						if string.sub(item.value, 1, 1) ~= "/" then
+							value = harpoon_key .. "/" .. item.value
+						end
+
+						if value == path then
+							vim.print(path)
+							return {
+								text = string.format(" ⥤ %d", i), -- <-- Add your favorite harpoon like arrow here
+								highlight = config.highlight or "NeoTreeDirectoryIcon",
+							}
+						end
+					end
+					return {}
+				end,
+			},
+			renderers = {
+				file = {
+					{ "icon" },
+					{ "name", use_git_status_colors = true },
+					{ "harpoon_index" }, --> This is what actually adds the component in where you want it
+					{ "diagnostics" },
+					{ "git_status", highlight = "NeoTreeDimText" },
+				},
+			},
+		},
+	},
 }
--- nvim/.config/nvim/lua/plugins/yazi.lua
+++ nvim/.config/nvim/lua/plugins/yazi.lua
@@ -9,7 +9,7 @@ return {
 	keys = {
 		-- 👇 in this section, choose your own keymappings!
 		{
-			"<leader>ft",
+			"<leader>y",
 			mode = { "n", "v" },
 			"<cmd>Yazi<cr>",
 			desc = "Open yazi at the current file",
@@ -29,7 +29,7 @@ return {
 	---@type YaziConfig | {}
 	opts = {
 		-- if you want to open yazi instead of netrw, see below for more info
-		change_neovim_cwd_on_close = true,
+		-- change_neovim_cwd_on_close = true,
 		open_for_directories = true,
 		keymaps = {
 			show_help = "<f1>",
