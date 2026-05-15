b710a85e08dac766a3c795be1ff400241d3be8ae
Cave-Johnson46 <rjcox46@yahoo.com>
2026-05-15 10:14:55 -0500

    Update

--- nvim/.config/nvim/lua/config/keybinds.lua
+++ nvim/.config/nvim/lua/config/keybinds.lua
@@ -11,6 +11,9 @@ vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
@@ -18,5 +21,5 @@ vim.keymap.set("i", "{<cr>", "{<cr>}<ESC>kA<CR>", {})
 closing_pairs = { "}", ")", "]" }
 opening_pairs = { "{", "(", "[" }
 for key, chr in pairs(opening_pairs) do
-	vim.keymap.set("i", chr, chr .. closing_pairs[key] .. "<esc>i", {})
+  vim.keymap.set("i", chr, chr .. closing_pairs[key] .. "<esc>i", {})
 end
